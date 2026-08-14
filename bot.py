f204bdfd8971a4123bbbb2b3a8c9b885beeb9659
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-08-14 15:07:52 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+from telegram.ext import (
+    Application,
+    CommandHandler,
+)
+
+from config.config import BOT_TOKEN
+
+from commands import (
+    start,
+    help,
+    status,
+    homepage,
+    fnnas,
+    portainer,
+    sftpgo,
+    frigate,
+)
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start.command))
+app.add_handler(CommandHandler("help", help.command))
+app.add_handler(CommandHandler("status", status.command))
+app.add_handler(CommandHandler("homepage", homepage.command))
+app.add_handler(CommandHandler("fnnas", fnnas.command))
+app.add_handler(CommandHandler("portainer", portainer.command))
+app.add_handler(CommandHandler("sftpgo", sftpgo.command))
+app.add_handler(CommandHandler("frigate", frigate.command))
+
+app.run_polling()

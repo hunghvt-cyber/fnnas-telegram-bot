916de1179f4a03b556295ff34bb36831f06d0923
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-08-14 10:02:56 +0700

    Create services/logger.py

---
+++ services/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+from config.config import LOG_FILE
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s"
+)
+
+logger = logging.getLogger("telegrambot")

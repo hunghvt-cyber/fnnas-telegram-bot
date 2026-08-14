9362e1223c1abb6661895446b82347cabe2ca117
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-08-14 09:59:27 +0700

    Create config/config.py

---
+++ config/config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ALLOWED_USER_ID = int(os.getenv("ALLOWED_USER_ID", "0"))
+
+STATUS_FILE = os.getenv(
+    "STATUS_FILE",
+    "/app/data/status.env"
+)
+
+LOG_FILE = os.getenv(
+    "LOG_FILE",
+    "/app/logs/bot.log"
+)

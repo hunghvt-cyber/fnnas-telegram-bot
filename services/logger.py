85d43edaeeb9343b90c8f2604437c4acbf2ee012
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-08-14 15:51:12 +0700

    Update logger.py

--- services/logger.py
+++ services/logger.py
@@ -5,7 +5,7 @@ from config.config import LOG_FILE
 logging.basicConfig(
     filename=LOG_FILE,
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(message)s"
+    format="%(asctime)s | %(levelname)s | %(message)s"
 )
 
-logger = logging.getLogger("telegrambot")
+logger = logging.getLogger("FnNASBot")

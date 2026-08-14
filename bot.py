603dbf644b5bdb779941b06bd1de4e4dbe7b41ff
hunghvt-cyber <thanhhunga4.wtf@gmail.com>
2026-08-14 23:15:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,12 @@ from telegram.ext import Application, CommandHandler
 from config.config import BOT_TOKEN
 from commands.start import command as start_command
 from commands.help import command as help_command
-from commands.status import command as status_command
 from commands.url import command as url_command
 
 app = Application.builder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start_command))
 app.add_handler(CommandHandler("help", help_command))
-app.add_handler(CommandHandler("status", status_command))
 app.add_handler(CommandHandler("url", url_command))
 
 app.run_polling()
